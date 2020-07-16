047f368b26eaaaf5b40a4e5d516e0c8c6d14dca1
ljylzc999867921 <2291014880@qq.com>
2020-07-16 16:11:35 +0800

    添加闪屏完成

---
+++ app/src/main/java/com/example/happytravel/SplashActivity.java
@@ -0,0 +1,52 @@
+package com.example.happytravel;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.RelativeLayout;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private RelativeLayout mSplashImg;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+         initView();
+        initSplash();
+    }
+
+    private void initView() {
+        mSplashImg = this.findViewById(R.id.splashBg);
+    }
+
+    private void initSplash() {
+        AlphaAnimation alphaAnimation=new AlphaAnimation(5f,1f);
+        alphaAnimation.setDuration(3000);
+        mSplashImg.setAnimation(alphaAnimation);
+        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent=new Intent(SplashActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
