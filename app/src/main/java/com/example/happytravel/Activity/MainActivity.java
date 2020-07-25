c5ceb44ac25a4fdbbf28ba33969f4506323d7e09
e <ya00ya11@126.com>
2020-07-25 15:12:14 +0800

    添加导航栏

--- app/src/main/java/com/example/happytravel/Activity/MainActivity.java
+++ app/src/main/java/com/example/happytravel/Activity/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.happytravel.Activity;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
+import com.example.happytravel.BlankFragment;
 import com.example.happytravel.Datas;
 import com.example.happytravel.adapters.ListViewAdapter;
 import com.example.happytravel.bean.ItemBean;
@@ -17,8 +24,13 @@ import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-private static final String TAG="MainActivity";
-private  RecyclerView mList;
+    private static final String TAG="MainActivity";
+    private  RecyclerView mList;
+    private ViewPager mViewPager;
+    private RadioGroup mTabRadioGroup;
+
+    private List<Fragment> mFragments;
+    private FragmentPagerAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,7 +60,78 @@ private  RecyclerView mList;
     }
 
     private void initView() {
+        // find view
+        mViewPager = findViewById(R.id.fragment_vp);
+        mTabRadioGroup = findViewById(R.id.tabs_rg);
+        // init fragment
+        mFragments = new ArrayList<>(4);
+        mFragments.add(BlankFragment.newInstance("这里是首页"));
+        mFragments.add(BlankFragment.newInstance("这里是记录"));
+        mFragments.add(BlankFragment.newInstance("这里是订单"));
+        mFragments.add(BlankFragment.newInstance("这里是我的"));
+        // init view pager
+        mAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager(), mFragments);
+        mViewPager.setAdapter(mAdapter);
+        // register listener
+        mViewPager.addOnPageChangeListener(mPageChangeListener);
+        mTabRadioGroup.setOnCheckedChangeListener(mOnCheckedChangeListener);
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mViewPager.removeOnPageChangeListener(mPageChangeListener);
+    }
+
+    private ViewPager.OnPageChangeListener mPageChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            RadioButton radioButton = (RadioButton) mTabRadioGroup.getChildAt(position);
+            radioButton.setChecked(true);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+    private RadioGroup.OnCheckedChangeListener mOnCheckedChangeListener = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            for (int i = 0; i < group.getChildCount(); i++) {
+                if (group.getChildAt(i).getId() == checkedId) {
+                    mViewPager.setCurrentItem(i);
+                    return;
+                }
+            }
+        }
+    };
+
+    private class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
+        private List<Fragment> mList;
+
+        public MyFragmentPagerAdapter(FragmentManager fm, List<Fragment> list) {
+            super(fm);
+            this.mList = list;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return this.mList == null ? null : this.mList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return this.mList == null ? 0 : this.mList.size();
+        }
     }
 
 
